1f3494209f9f509a74c40f3886bed30c0ee56b6c
MRasch1 <mrasch1102@gmail.com>
2024-06-07 09:59:18 +0200

    first commit

---
+++ test1.py
@@ -0,0 +1,8 @@
+# print("Hello World")
+
+# name=input("Give me your name: ")
+# print("Hello,", name)
+
+for i in range(10):
+    print("Hello")
+print("Bye!")
---
+++ Øvelse_2/Øvelse_2.py
@@ -0,0 +1,2 @@
+countryName=input("What country are you from?: ")
+print("I heard that", countryName, "is a beautiful country")    
